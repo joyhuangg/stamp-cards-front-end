7e03ccfa1f36cdbcb9463550a22c1f2f1dc905a8
joyhuangg <joyhuangg@gmail.com>
2018-10-19 16:02:32 -0400

    Fixed map bug

--- src/containers/DealCollection.js
+++ src/containers/DealCollection.js
@@ -3,7 +3,9 @@ import DealCard from '../components/DealCard'
 
 const DealCollection = (props) => {
   const {deals, clickDeal, store} = props
-  const renderDeals = deals.length > 0 ? deals.filter(deal => deal.store_id === store.id).map(deal => <DealCard key={deal.id} deal={deal} clickDeal={clickDeal}/>) : "No Deals Available"
+  const filteredDeals = deals.filter(deal => deal.store_id === store.id)
+  let renderDeals;
+  filteredDeals.length > 0 ? renderDeals = filteredDeals.map(deal => <DealCard key={deal.id} deal={deal} clickDeal={clickDeal}/>) : renderDeals = "No Deals Available"
 
 
   return (
